d9c088d07e7bff86e0b8d69f7783b4225f551765
Jeff Jiang <jeff.jiang13@gmail.com>
2024-06-09 14:52:52 -0400

    first commit

--- start-ngrok.ts
+++ start-ngrok.ts
@@ -3,16 +3,14 @@ import ngrok from 'ngrok';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    console.log('Handler invoked');
     const url = await ngrok.connect({
       authtoken: process.env.NGROK_AUTHTOKEN,
-      addr: 'http://localhost:3000',  // Use HTTPS as your local server uses HTTPS
+      addr: 'https://localhost:3000', // Ensure this is your local service
       subdomain: 'neat-decent-mongrel'
     });
-    console.log('Ngrok URL:', url);
+
     res.status(200).json({ url });
   } catch (error) {
-    console.error('Error starting ngrok:', error);
     res.status(500).json({ error: 'Failed to start ngrok' });
   }
 }
