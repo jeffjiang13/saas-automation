1c6fd59fc722a24500ece91f24c69f82d1e6fab9
Jeff Jiang <jeff.jiang13@gmail.com>
2024-06-08 20:41:23 -0400

    first commit

---
+++ src/app/api/start-ngrok.tsx
@@ -0,0 +1,17 @@
+// src/app/api/start-ngrok.ts
+import type { NextApiRequest, NextApiResponse } from 'next';
+import ngrok from 'ngrok';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const url = await ngrok.connect({
+      authtoken: process.env.NGROK_AUTHTOKEN,
+      addr: 'https://jj-zapflow.vercel.app',
+      subdomain: 'neat-decent-mongrel'
+    });
+
+    res.status(200).json({ url });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to start ngrok' });
+  }
+}
