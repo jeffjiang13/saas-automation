07651c16c77d2780e5010579cc075f1e08a5b92e
Jeff Jiang <jeff.jiang13@gmail.com>
2024-06-10 04:03:14 -0400

    working on cron

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,18 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { exec } from 'child_process';
-import { promisify } from 'util';
-
-const execAsync = promisify(exec);
 
 export async function GET(req: NextRequest): Promise<Response> {
   try {
-    // Start ngrok
-    await execAsync('ngrok start --all --config=ngrok.yml');
-    console.log('Ngrok started');
-
-    // Your logic here, for example, triggering some task
-    // You can also include logic to stop ngrok after the task is done if needed
-
+    // Your logic here
     return NextResponse.json({ message: 'Cron job executed successfully' });
   } catch (error) {
     console.error(error);
