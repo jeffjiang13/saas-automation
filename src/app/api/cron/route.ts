a2762d22b4a3755707873abf0f8d594c1d197cb9
Jeff Jiang <jeff.jiang13@gmail.com>
2024-06-10 02:45:42 -0400

    working on cron

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -6,7 +6,7 @@ export async function GET(req: NextRequest): Promise<Response> {
     exec('ngrok start --none --log=stdout --authtoken=$NGROK_AUTHTOKEN', (error, stdout, stderr) => {
       if (error) {
         console.error(`Error: ${stderr}`);
-        resolve(NextResponse.json({ error: 'Failed to start ngrok' }, { status: 500 }));
+        resolve(NextResponse.json({ error: 'Failed to start ngrok', details: stderr }, { status: 500 }));
       } else {
         resolve(NextResponse.json({ url: stdout.trim() }));
       }
