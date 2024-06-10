5894a2241ab42c2f663229af426ea76077010d80
Jeff Jiang <jeff.jiang13@gmail.com>
2024-06-10 01:57:48 -0400

    working on cron

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,16 +1,16 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
 import ngrok from 'ngrok';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest) {
   try {
     const url = await ngrok.connect({
       authtoken: process.env.NGROK_AUTHTOKEN,
-      addr: 'http://localhost:3000', // Keep this as is for local dev
+      addr: 'https://localhost:3000',
       subdomain: 'neat-decent-mongrel'
     });
 
-    res.status(200).json({ url });
+    return NextResponse.json({ url });
   } catch (error) {
-    res.status(500).json({ error: 'Failed to start ngrok' });
+    return NextResponse.json({ error: 'Failed to start ngrok' }, { status: 500 });
   }
 }
