ac38a1b4e609a99c3fead7f5bbd3ca411bbc35b2
Jeff Jiang <jeff.jiang13@gmail.com>
2024-06-23 00:25:53 -0400

    finished clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
         <body className={font.className}>
           <ThemeProvider
             attribute="class"
-            defaultTheme="dark"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
